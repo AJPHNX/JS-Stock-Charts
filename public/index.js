5c67b37015602257c33d346b1d2f2a4546ce75ff
ajphnx <ajph03nix@gmail.com>
2022-04-27 19:49:09 -0400

    final assignment

--- public/index.js
+++ public/index.js
@@ -1,19 +1,64 @@
+/*******
+ * Returns color value associated with symbol
+**********/
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
+/*******
+ * Get highest stock values in value array
+**********/
+function getHighest(values){
+    let highest = 0
+    values.forEach(value =>{
+        if(value.high > highest){
+            highest = parseFloat(value.high)
+        }
+    })
+    return highest
+}
+/*******
+ * Gets average value array
+**********/
+function getAverage(values){
+    let total = 0
+    values.forEach(value =>{
+        total += parseFloat(value.high)
+    })
+    return total / values.length
+}
+/*******
+ * Main
+**********/
 async function main() {
+    //**Declarations**//
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
     const apiKey ='07ce928a4b86466ca8d8acd05775a423'
-    // const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=${apiKey}`)
+    const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=${apiKey}`)
     
-    //const responseJson = await response.json()
+    const responseJson = await response.json()
 
-    const { GME, MSFT, DIS, BNTX } = mockData;
+    const { GME, MSFT, DIS, BNTX } = responseJson;
 
     const stocks = [GME, MSFT, DIS, BNTX];
+    //************************//
     stocks.forEach(stock => stock.values.reverse())
-    // xnes 
-   
-    // var ctx = document.getElementById('myChart').getContext('2d');
+    /*******
+     * Chart:1
+     * Price over time
+     **********/
     new Chart(timeChartCanvas.getContext('2d'), {
         type: 'line',
         data: {
@@ -27,6 +72,10 @@ async function main() {
               
         }
     });
+     /*******
+     * Chart:2
+     * Highest Stock Price
+     **********/
     new Chart(highestPriceChartCanvas.getContext('2d'), {
         type: 'bar',
         data: {
@@ -45,6 +94,10 @@ async function main() {
             }]
         }
     });
+     /*******
+     * Chart:3
+     * Average Stock Price
+     **********/
     new Chart(averagePriceChartCanvas.getContext('2d'), {
         type: 'pie',
         data: {
@@ -63,41 +116,6 @@ async function main() {
             }]
         }
     });
-
-    function getColor(stock){
-        if(stock === "GME"){
-            return 'rgba(61, 161, 61, 0.7)'
-        }
-        if(stock === "MSFT"){
-            return 'rgba(209, 4, 25, 0.7)'
-        }
-        if(stock === "DIS"){
-            return 'rgba(18, 4, 209, 0.7)'
-        }
-        if(stock === "BNTX"){
-            return 'rgba(166, 43, 158, 0.7)'
-        }
-    }
-    function getHighest(values){
-        let highest = 0
-        values.forEach(value =>{
-            if(value.high > highest){
-                highest = parseFloat(value.high)
-            }
-        })
-        return highest
-    }
-    function getAverage(values){
-        let total = 0
-        values.forEach(value =>{
-            total += parseFloat(value.high)
-        })
-        return total / values.length
-    }
-
-
-// console.log(stocks[0].values)
-// stock[0].values.map(value => value.datetime)
 
 }
 
